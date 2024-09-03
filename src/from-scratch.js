fe0a0ffcabd69dffdffdff7959e644dac3079edf
electrxety <er191712@gmail.com>
2024-09-03 15:34:06 -0400

    solved q5: handled errors

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,7 +1,13 @@
 //3.0.2 Assignment: Async/Await by Eileen
 
-//QUESTION 1-4: this func is going to be a wrapper for the fetch api- 1) pass in the arguments (a url str + an options obj), 2) make the func async, 3) return a tuple, 4) make the fetch call
+//QUESTION 1-5: this func is going to be a wrapper for the fetch api- 1) pass in the arguments (a url str + an options obj), 2) make the func async, 3) return a tuple, 4) make the fetch call, 5) check for errors
 export const fetchHandler = async (url, options = {}) => {
-  const response = await fetch(url, options);
-  return [{}, null]; //returning a tuple to handle data + errors in a predictable way (order never changes) beyond this func if needed
+  try {
+    const response = await fetch(url, options);
+    return [{}, null]; //returning a tuple of the data (tuples handle data + errors in a predictable way as the order never changes so we can use them beyond helper fetch funcs if needed)
+ }
+  catch (err) {
+    console.warn(err);
+    return [null, err]; //returning a tuple of the error
+ }
 };
