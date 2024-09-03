49fb8fdb88933228d635d6c83a369ab56caf346e
electrxety <er191712@gmail.com>
2024-09-03 15:31:56 -0400

    solved q2: made func async

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,5 @@
 //3.0.2 Assignment: Async/Await by Eileen
 
-//QUESTION 1: this func is going to be a wrapper for the fetch api- 1) pass in the arguments (a url str + an options obj)
-export const fetchHandler = (url, options = {}) => {
+//QUESTION 1-2: this func is going to be a wrapper for the fetch api- 1) pass in the arguments (a url str + an options obj), 2) make the func async
+export const fetchHandler = async (url, options = {}) => {
 };
