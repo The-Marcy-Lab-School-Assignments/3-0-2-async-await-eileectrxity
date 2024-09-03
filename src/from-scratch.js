6355835905ee1615b0aab00927f74f5a550cd090
electrxety <er191712@gmail.com>
2024-09-03 15:32:35 -0400

    solved q3: returned a tuple

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,6 @@
 //3.0.2 Assignment: Async/Await by Eileen
 
-//QUESTION 1-2: this func is going to be a wrapper for the fetch api- 1) pass in the arguments (a url str + an options obj), 2) make the func async
+//QUESTION 1-3: this func is going to be a wrapper for the fetch api- 1) pass in the arguments (a url str + an options obj), 2) make the func async, 3) return a tuple
 export const fetchHandler = async (url, options = {}) => {
+  return [{}, null]; //returning a tuple to handle data + errors in a predictable way (order never changes) beyond this func if needed
 };
