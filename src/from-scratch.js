f7213b4d62d07a7206d41df2a6e1a7c868be2aee
electrxety <er191712@gmail.com>
2024-09-03 15:31:22 -0400

    solved q1: passed in args

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,2 +1,5 @@
-export const fetchHandler = () => {
+//3.0.2 Assignment: Async/Await by Eileen
+
+//QUESTION 1: this func is going to be a wrapper for the fetch api- 1) pass in the arguments (a url str + an options obj)
+export const fetchHandler = (url, options = {}) => {
 };
