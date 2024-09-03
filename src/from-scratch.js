2cdaa85ca5289a00c0b609b6cef064513f309cb8
electrxety <er191712@gmail.com>
2024-09-03 15:33:08 -0400

    solved q4: made fetch call

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,6 +1,7 @@
 //3.0.2 Assignment: Async/Await by Eileen
 
-//QUESTION 1-3: this func is going to be a wrapper for the fetch api- 1) pass in the arguments (a url str + an options obj), 2) make the func async, 3) return a tuple
+//QUESTION 1-4: this func is going to be a wrapper for the fetch api- 1) pass in the arguments (a url str + an options obj), 2) make the func async, 3) return a tuple, 4) make the fetch call
 export const fetchHandler = async (url, options = {}) => {
+  const response = await fetch(url, options);
   return [{}, null]; //returning a tuple to handle data + errors in a predictable way (order never changes) beyond this func if needed
 };
